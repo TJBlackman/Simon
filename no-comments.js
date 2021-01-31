595b54d10c6bd0128f700aa004265d19441473a6
Trevor Blackman <TJBlackman08@gmail.com>
2021-01-31 11:03:39 -0700

    feat: trevor's game

--- no-comments.js
+++ no-comments.js
@@ -1,4 +1,3 @@
-// save refences to HTML elements
 const h3 = $('h3');
 const arrowUp = $('#btn-up');
 const arrowLeft = $('#btn-left');
@@ -6,24 +5,19 @@ const arrowDown = $('#btn-down');
 const arrowRight = $('#btn-right');
 const repeatPatternBtn = $('#repeat-pattern-btn');
 
-// initialize sounds
 const audioUp = new Audio(`/sounds/arrow-up.mp3`);
 const audioLeft = new Audio(`/sounds/arrow-left.mp3`);
 const audioDown = new Audio(`/sounds/arrow-down.mp3`);
 const audioRight = new Audio(`/sounds/arrow-right.mp3`);
 const audioError = new Audio(`/sounds/wrong.mp3`);
 
-// game settings
 let pattern = [];
 let userPositionInPattern = 0;
 
-// function to determine which key was pressed OR clicked
-// take in data, return up, left, down, right, undefined
-// this uses the "fall-through" technique
 function getDirectionFromInput(data) {
   let direction = undefined;
   switch (data) {
-    case 38: // 1st "fall through"
+    case 38:
     case 'btn-up': {
       direction = 'up';
       break;
@@ -50,7 +44,6 @@ function getDirectionFromInput(data) {
   return direction;
 }
 
-// randomly return up, left, down, right
 function generateRandomDirection() {
   const number = Math.random();
   let key = 'left';
@@ -110,11 +103,8 @@ function emphasizeKeyPress(direction) {
   }
 }
 
-// accept an array as a paramter
-// loop over array, click button depending on array value
-// don't register clicks as user input, we're just playing the pattern
 function playPattern(array) {
-  const interval = 333; // 0.5s
+  const interval = 333;
   for (let i = 0; i < array.length; ++i) {
     setTimeout(() => {
       emphasizeKeyPress(array[i]);
@@ -122,8 +112,6 @@ function playPattern(array) {
   }
 }
 
-// add random direction to pattern
-// reset userInput
 function incrementLevel() {
   h3.text(`Level ${pattern.length}`);
   const direction = generateRandomDirection();
@@ -134,12 +122,10 @@ function incrementLevel() {
   }, 500);
 }
 
-// compare items from arrays
 function isInputCorrect(direction) {
   return direction === pattern[patternIndex];
 }
 
-// show game is over, reset patterns to be empty
 function showGameOver() {
   audioError.currentTime = 0;
   audioError.play();
@@ -155,13 +141,9 @@ function showLevelCompleted() {
   }, 250);
 }
 
-// Main logic of game!!
 function registerSelection(direction) {
-  // when direction is selected, show btn press on screen
   emphasizeKeyPress(direction);
 
-  // if game not started, start it using this direction as first direction
-  // increment level, then return
   if (pattern.length === 0) {
     $(document.body).removeClass('game-over');
     showLevelCompleted();
@@ -170,28 +152,21 @@ function registerSelection(direction) {
     return;
   }
 
-  // if error, end game
   const correct = isInputCorrect(direction);
   if (!correct) {
     showGameOver();
     return;
   }
 
-  // if input is end of pattern, increment level
   if (pattern.length - 1 === patternIndex) {
     showLevelCompleted();
     incrementLevel();
     return;
   }
 
-  // increment patternIndex
-  // this keeps track of where in the pattern the user should be
   patternIndex += 1;
 }
 
-// listen for clicks on .btn elements
-// convert clicked key to up, down, left, right
-// register direction input
 $('.btn').click(function (e) {
   const htmlId = $(this).attr('id');
   const direction = getDirectionFromInput(htmlId);
@@ -200,9 +175,6 @@ $('.btn').click(function (e) {
   }
 });
 
-// listen for keydown event
-// convert keydown key to up, down, left, right
-// register direction input
 $(document).on('keydown', function (e) {
   const keyId = e.which;
   const direction = getDirectionFromInput(keyId);
@@ -211,14 +183,6 @@ $(document).on('keydown', function (e) {
   }
 });
 
-// on btn click, replay the pattern
 repeatPatternBtn.click(function () {
   playPattern(pattern);
 });
-
-// CHALLENGES:
-// - open "no-comments.js" and add in your own comments to explain the purpose of function
-// - limit "Replay Pattern" button to only 2 uses per game
-// - increase pattern playback speed by small amount for each additional level
-//    - pattern playback at start of each level gets faster and faster
-// - use a cookie, or localStorage to store high-score that persists even when a user refreshes the page
